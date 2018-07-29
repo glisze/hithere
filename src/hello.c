a04014b6ff41865f4782ad2b02ed341e25034c93
Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>
2018-07-29 21:36:01 +0100

    hithere, version: taking the low road; it works, but...
    Signed-off-by: Gunter Liszewski (s70-11) <gunter.liszewski@gmail.com>

--- src/hello.c
+++ src/hello.c
@@ -1,11 +1,14 @@
 #include <config.h>
 #include <stdio.h>
+#include "../lib/version-etc.h"
 
 int
 main(void)
 {
   puts ("Hi there!");
   /*  puts ("This is " PACKAGE_STRING "."); */
+  version_etc( stdout, 0, PACKAGE, VERSION,  "Gunter Liszewski", NULL);
+  
   emit_bug_reporting_address();
   return 0;
 }
